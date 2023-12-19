6a6175bcb3a893c457b4d8acda5ee35f2a2484ef
shree <anirudhamane45@gmail.com>
2023-12-18 18:03:20 -0800

    first try

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import {usestate} from "react";
 
 const App = () => {
+
+  const [name,setName]=usestate('');
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text" onChange={(e)=> setName(e.target.value)}></input>
+        <p>Hello {name}!</p>
     </div>
   )
 }
